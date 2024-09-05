6ff5a7a45358bc34e5344301505929c43694851f
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-09-05 15:45:23 +0200

    add derictories: editorial_office and registration to templates

--- editorial_office/forms.py
+++ editorial_office/forms.py
@@ -1,39 +1,88 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.urls import reverse
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from editorial_office.models import Newspaper, Redactor, Topic
 
 
-class Topic(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    class Meta:
-        ordering = ["name"]
-
-    def __str__(self):
-        return self.name
-
-
-class Redactor(AbstractUser):
-    years_of_experience = models.IntegerField(default=0)
+class NewspaperForm(forms.ModelForm):
+    topics = forms.ModelMultipleChoiceField(
+        queryset=Topic.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
 
     class Meta:
-        verbose_name = "publisher"
-        verbose_name_plural = "publishers"
-
-    def __str__(self):
-        return f"{self.username} ({self.first_name} {self.last_name})"
-
-    def get_absolute_url(self):
-        return reverse("editorial_office:publisher-detail", kwargs={"pk": self.pk})
+        model = Newspaper
+        fields = "__all__"
 
 
-class Newspaper(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(max_length=2000)
-    published_date = models.DateField(auto_now_add=True)
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    publishers = models.ManyToManyField(Redactor, related_name="newspapers", blank=True)
+class RedactorCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "year_of_experience",
+            "first_name",
+            "last_name",
+        )
 
-    def __str__(self):
-        return f"{self.title} ({self.published_date})"
+    def clean_year_of_experience(self):  # this logic is optional, but possible
+        return validate_year_of_experience(self.cleaned_data["year_of_experience"])
 
+
+class RedactorLicenseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Redactor
+        fields = ["year_of_experience"]
+
+    def clean_year_of_experience(self):
+        return validate_year_of_experience(self.cleaned_data["year_of_experience"])
+
+
+def validate_year_of_experience(
+        year_of_experience,
+):  # regex validation is also possible here
+    if len(year_of_experience) != 0:
+        raise ValidationError("should consist more of 0 characters")
+    elif not year_of_experience.isdigit():
+        raise ValidationError("characters should be digits")
+
+    return year_of_experience
+
+
+class NewspaperSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "search by title"
+            }
+        ),
+    )
+
+
+class RedactorSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "search by username"
+            }
+        ),
+    )
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "search by name"
+            }
+        ),
+    )
